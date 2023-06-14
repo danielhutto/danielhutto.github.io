999650953a052e64e264958fd55e1406cee7eaf9
danielhutto <daniel.hutto.tx@gmail.com>
2023-06-13 19:08:37 -0500

    Delete pdf_viewer.php

--- pdf_viewer.php
+++
@@ -1,9 +0,0 @@
-<?php
-$pdfFile = 'files\DanielHuttoResume.pdf';
-
-// Generate HTML code to open the PDF file in a new window/tab
-echo '<a href="' . $pdfFile . '" target="_blank">Click here to open the PDF</a>';
-
-// Generate HTML code to embed the PDF file on the webpage
-echo '<embed src="' . $pdfFile . '" type="application/pdf" width="100%" height="600px" />';
-?>
